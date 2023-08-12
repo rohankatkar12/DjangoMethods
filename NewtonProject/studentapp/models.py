cbfa5ab479c2b823fb7915c85569f97f23376cd2
Rohan Katkar <rohankatkar1212@gmail.com>
2023-08-13 00:02:05 +0530

    done some migrations

--- NewtonProject/studentapp/models.py
+++ NewtonProject/studentapp/models.py
@@ -17,6 +17,13 @@ class StudentId(models.Model):
         return self.student_id
     
 
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=100)
+
+    def __str__(self) ->str:
+        return self.subject_name
+    
+    
 class Student(models.Model):
     department = models.ForeignKey(Department, related_name='depart', on_delete=models.CASCADE)
     student_id = models.OneToOneField(StudentId, related_name='studentid', on_delete=models.CASCADE)
@@ -33,4 +40,14 @@ class Student(models.Model):
         verbose_name = "Student"
        
     
-     
+
+class SubjectMarks(models.Model):
+    student = models.ForeignKey(Student, related_name='studentmarks', on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    marks = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.student.student_name} {self.subject.subject_name}'
+
+    class Meta:
+        unique_together = ['student', 'subject']
